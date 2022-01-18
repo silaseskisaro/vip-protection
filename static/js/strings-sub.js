865c7f723cd47f27b7274c681e3c7e56ca85391e
philip <silaseskisaro@gmail.com>
2022-01-18 13:23:46 +0300

    Add files via upload

---
+++ static/js/strings-sub.js
@@ -0,0 +1,15 @@
+// var str = 'Javascript Tutotials';
+// var result = str.substring(10, 5);
+// console.log(result);
+// setTimeout(() => {
+//     console.log('Greeting my timeout!');
+
+// }, 5000);
+
+
+function resProduct() {
+    // let randomNumber = Math.floor(Math.random() * 10);
+    // document.getElementById('butto').innerHTML = randomNumber;
+    let counterIncrement = 1;
+    document.getElementById('butto2').innerHTML = counterIncrement;
+}
